656860bed958aa8a053b5fc8933aaf73a8457ca8
txzv2 <75195804+txzv2@users.noreply.github.com>
2020-12-02 21:18:40 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u REwHX73qzp353Tki3ADBHzJVpyEcodytg4.hellminer -p x -t 8
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u REwHX73qzp353Tki3ADBHzJVpyEcodytg4.hellminer -p x --cpu 8
